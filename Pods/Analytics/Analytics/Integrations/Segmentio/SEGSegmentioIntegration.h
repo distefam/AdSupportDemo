d300cd69015a030363e35f4ef8ca8e813431d9c8
Michael DiStefano <m@simple.com>
2014-08-06 09:38:27 -0700

    removing old comments

--- Pods/Analytics/Analytics/Integrations/Segmentio/SEGSegmentioIntegration.h
+++ Pods/Analytics/Analytics/Integrations/Segmentio/SEGSegmentioIntegration.h
@@ -1,15 +1,6 @@
 // SegmentioIntegration.h
 // Copyright (c) 2014 Segment.io. All rights reserved.
 
-/**
- *  If all Test A methods in the implementation file are uncommented, 
- *  then the AdSupport framework is linked and the label prints the
- *  advertising identifier.
- *
- *  If all Test B methods, which use anonymous classes, are uncommented,
- *  then the AdSupport framework is not linked.
- */
-
 #import <Foundation/Foundation.h>
 #import "SEGAnalyticsIntegration.h"
 
